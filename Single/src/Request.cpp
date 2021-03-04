fd9d37dadc744bebdc3a2eb0296122df63a954b8
Eshita Bhargava <58271887+Eshitabhargava@users.noreply.github.com>
2021-03-04 16:33:40 +0530

    Updated Request.cpp

--- Single/src/Request.cpp
+++ Single/src/Request.cpp
@@ -14,5 +14,15 @@ this->forwardTo=forwardTo;
 
 string Request::get(string name)
 {
-return string("2"); //Temporarily hard-coded
+string res;
+int row,col;
+for(row=0;row<this->dataCount;++row)
+{
+for(col=0;data[row][col] && data[row][col]!='=';++col);
+if(data[row][col]!='=') continue;
+if(strncmp(data[row],name.c_str(),col)==0) break;
+}
+if(row==this->dataCount) res="";
+else res=string(data[row]+(col+1));
+return res;
 }
